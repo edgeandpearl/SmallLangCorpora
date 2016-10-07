76cf3dfb360e47bd8bb01a16abed850aaa072d84
edgeandpearl <tanglorfirefoot@gmail.com>
2016-10-07 15:05:12 +0300

    dummy login page & stuff

--- smallangs.py
+++ smallangs.py
@@ -45,4 +45,16 @@ def query():
 def empty():
     return render_template("empty.html", args=request.args.getlist('collection'))
 
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect("/")
+    return render_template('login.html', error=error)
+
+
 app.run(debug=True)
